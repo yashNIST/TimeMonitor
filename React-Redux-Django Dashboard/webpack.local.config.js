c7b6915526acf81e3d0fc426e699bf7aaf0cb93e
kevingbrady <kgb@nist.gov>
2017-09-14 16:29:31 -0400

    Add files via upload

---
+++ React-Redux-Django Dashboard/webpack.local.config.js
@@ -0,0 +1,39 @@
+/**
+ * Created by kgb on 9/12/17.
+ */
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+var config = require('./webpack.base.config.js')
+
+config.devtool = "#eval-source-map"
+
+var ip = 'localhost'
+
+config.entry = {
+  Timing_Testbed_Dashboard: [
+    'webpack-dev-server/client?http://' + ip + ':3000',
+    'webpack/hot/only-dev-server',
+    './ReactJS/Dashboard',
+  ],
+}
+
+config.output.publicPath = 'http://' + ip + ':3000' + '/Dashboard/static/bundles/local/'
+
+config.plugins = config.plugins.concat([
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new BundleTracker({filename: './webpack-stats-local.json'}),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('development'),
+      'BASE_API_URL': JSON.stringify('https://'+ ip +':8000/'),
+  }}),
+])
+
+config.module.loaders.push(
+  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
+)
+
+module.exports = config
---
+++ React-Redux-Django Dashboard/webpack.prod.config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by kgb on 9/12/17.
+ */
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+var config = require('./webpack.base.config.js')
+
+config.output.path = require('path').resolve('./Dashboard/static/bundles/prod/')
+
+config.plugins = config.plugins.concat([
+  new BundleTracker({filename: './webpack-stats-prod.json'}),
+
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+      'BASE_API_URL': JSON.stringify('https://http://127.0.0.1:8000/production'),
+  }}),
+
+  // keeps hashes consistent between compilations
+  new webpack.optimize.OccurenceOrderPlugin(),
+
+  // minifies your code
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+])
+
+// Add a loader for JSX files
+config.module.loaders.push(
+  { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' }
+)
+
+module.exports = config
