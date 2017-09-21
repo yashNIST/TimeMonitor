1555568d5a5b95f25a68a0c154003148f127303c
kevingbrady <kgb@nist.gov>
2017-09-21 12:48:35 -0400

    Updated Dashboard with just React no Redux

--- React-Redux-Django Dashboard/ReactJS/Navbar.js
+++ React-Redux-Django Dashboard/ReactJS/Navbar.js
@@ -3,8 +3,8 @@
  */
 
 const React = require("react");
-import { FilterLink } from "./containers/FilterLink"
-
+import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap';
 const ReactBootstrap = require('react-bootstrap');
 let Navbar = ReactBootstrap.Navbar;
 let NavItem = ReactBootstrap.NavItem;
@@ -21,25 +21,45 @@ export default class NavigationBar extends React.Component{
 
             <Navbar inverse collapseOnSelect>
            <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Timing Testbed Dashboard</a>
+            <Navbar.Brand componentClass="span">
+              <Link activeClass="active" to="/">Timing Testbed Dashboard</Link>
             </Navbar.Brand>
            <Navbar.Toggle />
            </Navbar.Header>
            <Navbar.Collapse>
             <Nav pullLeft>
               <NavDropdown title="Announce Message Tests" id="Announce Dropdown">
-                  <NavItem onClick={() => <FilterLink to="/BMCA"/>}>BMCA</NavItem>
-                  <NavItem href="#">Holdover</NavItem>
-                  <NavItem href="#">Leap Second</NavItem>
-                  <NavItem href="#">ATOI</NavItem>
+                  <LinkContainer to="/BMCA">
+                  <NavItem>BMCA</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/Holdover">
+                  <NavItem>Holdover</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/LeapSecond">
+                  <NavItem>Leap Second</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/ATOI">
+                  <NavItem>ATOI</NavItem>
+                  </LinkContainer>
               </NavDropdown>
               <NavDropdown title="Path Delay Request Message Tests" id="Path Dropdown">
-                <NavItem href="#">Multicast MAC Address</NavItem>
+                <LinkContainer to="/MulticastMAC">
+                  <NavItem>Multicast MAC Address</NavItem>
+                  </LinkContainer>
               </NavDropdown>
               <NavDropdown title="Rest API" id="REST Dropdown">
-                <NavItem href="/All_Announce_Messages/">All Announce Messages</NavItem>
-                  <NavItem href="#">Individual Announce Messages</NavItem>
+                  <LinkContainer to="/All_Announce_Messages/">
+                   <NavItem>All Announce Messages</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/All_PDelay_Messages/">
+                   <NavItem>All PDelay Messages</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/Individual_Announce_Messages/1">
+                      <NavItem>Individual Announce Messages</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/Individual_PDelay_Messages/1">
+                      <NavItem>Individual PDelay Messages</NavItem>
+                  </LinkContainer>
               </NavDropdown>
             </Nav>
                <Navbar.Form pullRight>
@@ -58,77 +78,5 @@ export default class NavigationBar extends React.Component{
 
 /*
 
-
-            <nav className="navbar navbar-inverse">
-
-                <div className="container-fluid">
-
-                    <div className="navbar-header">
-
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#topNavBar">
-
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-
-                        </button>
-                        <p className="navbar-brand"><ReactRouterDOM.Link to="/" activeClassName="active">Timing Testbed Dashboard</ReactRouterDOM.Link></p>
-                    </div>
-                    <div className="collapse navbar-collapse" id="topNavBar">
-                        <ul className="nav navbar-nav">
-                            <li className="active"></li>
-                            <li className="dropdown">
-
-                                <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                   aria-expanded="false">Announce Message Tests<span className="caret"></span></a>
-                                   <ul className="dropdown-menu">
-                                       <li><ReactRouterDOM.Link to="/BMCA" activeClassName="active">BMCA</ReactRouterDOM.Link></li>
-                                       <li><ReactRouterDOM.Link to="/Holdover" activeClassName="active">Holdover</ReactRouterDOM.Link></li>
-                                       <li><ReactRouterDOM.Link to="/LeapSecond" activeClassName="active">Leap Second</ReactRouterDOM.Link></li>
-                                       <li><ReactRouterDOM.Link to="/ATOI" activeClassName="active">ATOI</ReactRouterDOM.Link></li>
-                                    </ul>
-
-                            </li>
-                            <li className="dropdown">
-
-                                <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                   aria-expanded="false" href="#">Path Delay Request Message Tests<span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-
-                                    <li><a onClick={()=>history.push('/MulticastMAC')}>Multicast MAC Address</a></li>
-                                </ul>
-
-                            </li>
-                            <li className="dropdown">
-
-                                <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                   aria-expanded="false" href="#">REST API<span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-
-                                    <li><a href="http://127.0.0.1:8000/All_Announce_Messages/">All Announce Messages</a>
-                                    </li>
-                                    <li><a href="http://127.0.0.1:8000/Individual_Announce_Messages/(?P<pk>[0-9]+)/">Individual
-                                        Announce Messages</a></li>
-                                </ul>
-
-                            </li>
-
-                        </ul>
-                        <form className="navbar-form navbar-left">
-
-                            <div className="form-group">
-
-                                <input type="text" className="form-control" name="q" value=""/>
-
-                            </div>
-                            <button type="submit" className="btn btn-default">Search</button>
-
-                        </form>
-
-                    </div>
-
-                </div>
-
-            </nav>
 
  */
