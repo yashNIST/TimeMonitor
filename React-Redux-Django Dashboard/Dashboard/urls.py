e45881eab953ea74c7952a88388e68395f69ccb9
kevingbrady <kgb@nist.gov>
2017-09-21 12:55:09 -0400

    Update urls.py

--- React-Redux-Django Dashboard/Dashboard/urls.py
+++ React-Redux-Django Dashboard/Dashboard/urls.py
@@ -6,11 +6,13 @@ app_name = 'Dashboard'
 urlpatterns = [
 
        url(r'^$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Dashboard.html')),
-       # url(r'^BMCA/$', views.Announce_MessageListView.as_view(template_name = "Timing_Testbed_Dashboard/BMCA/BMCA.html"), name='BMCA'),
-       # url(r'^LeapSecond/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/LeapSecond/LeapSecond.html'), name='LeapSecond'),
-       # url(r'^ATOI/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/ATOI/ATOI.html'), name='ATOI'),
-       # url(r'^Holdover/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Holdover/Holdover.html'), name='Holdover'),
-       # url(r'^MulticastMAC/$', views.P_Delay_Request_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Multicast_MAC_Address/MulticastMac.html'), name='MulticastMAC'),
-        url(r'^All_Announce_Messages/$', views.Announce_MessageRESTListView.as_view(), name='All_Announce_Messages'),
-        url(r'^Individual_Announce_Messages/(?P<pk>[0-9]+)/$', views.Announce_MessageRESTDetailView.as_view(), name='Individual_Announce_Messages'),
+       url(r'^BMCA/$', views.Announce_MessageListView.as_view(template_name = 'Timing_Testbed_Dashboard/Dashboard.html'), name='BMCA'),
+       url(r'^LeapSecond/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Dashboard.html'), name='LeapSecond'),
+       url(r'^ATOI/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Dashboard.html'), name='ATOI'),
+       url(r'^Holdover/$', views.Announce_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Dashboard.html'), name='Holdover'),
+       url(r'^MulticastMAC/$', views.P_Delay_Request_MessageListView.as_view(template_name='Timing_Testbed_Dashboard/Dashboard.html'), name='MulticastMAC'),
+       url(r'^All_Announce_Messages/$', views.Announce_MessageRESTListView.as_view(), name='All_Announce_Messages'),
+       url(r'^Individual_Announce_Messages/(?P<pk>[0-9]+)/$', views.Announce_MessageRESTDetailView.as_view(), name='Individual_Announce_Messages'),
+       url(r'^All_PDelay_Messages/$', views.PDelay_MessageRESTListView.as_view(), name='All_PDelay_Messages'),
+       url(r'^Individual_PDelay_Messages/(?P<pk>[0-9]+)/$', views.PDelay_MessageRESTDetailView.as_view(), name='Individual_PDelay_Messages'),
     ]
