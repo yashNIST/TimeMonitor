ee0436f804ea4af6bead62e0f5bb8496fe0c64bb
kevingbrady <kgb@nist.gov>
2017-09-21 12:47:47 -0400

    Updated Dashboard with just React no Redux

--- React-Redux-Django Dashboard/ReactJS/Dashboard.js
+++ React-Redux-Django Dashboard/ReactJS/Dashboard.js
@@ -1,8 +1,20 @@
 import { render } from "react-dom"
 const React = require("react");
-import {BrowserRouter, Switch, Route} from 'react-router'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import 'babel-polyfill';
 
+import NavigationBar from "./Navbar"
+import Announce_Message_Table from "./components/Announce_Message_Table"
+import BMCA from "./components/BMCA"
+import Holdover from  "./components/Holdover"
+import LeapSecond from "./components/LeapSecond"
+import ATOI from "./components/ATOI"
+import MulticastMAC from "./components/MulticastMAC"
+
+import * as announceMessageActions from "./actions/announceMessageActions"
+import announceTableContainer from "./containers/announceTableContainer"
+import HoldoverContainer from "./containers/HoldoverContainer"
+/*
 import {
   createStore,
   compose,
@@ -11,9 +23,6 @@ import {
 } from "redux"
 import { Provider } from "react-redux"
 import thunk from "redux-thunk"
-
-import DashboardContainer from "./containers/DashboardContainer"
-
 import * as reducers from "./reducers/reducers"
 
 let finalCreateStore = compose(
@@ -23,50 +32,103 @@ let finalCreateStore = compose(
 let reducer = combineReducers(reducers)
 let store = finalCreateStore(reducer)
 
+*/
+
 var Dashboard = React.createClass({
 
-    /*
     loadAnnounce_MessagesFromServer(){
+
         $.ajax({
-            url: this.props.url,
+            url: "All_Announce_Messages",
             datatype: 'json',
             cache: false,
-            success: function(data) {
-                this.setState({data: data});
+            success: function(announcemessages) {
+                this.setState({
+                    AnnounceMessages: announcemessages,
+                });
             }.bind(this)
         })
     },
 
-    getInitialState: function() {
-        return {data: []};
+    loadPDelay_MessagesFromServer(){
+
+        $.ajax({
+            url: "All_PDelay_Messages",
+            datatype: 'json',
+            cache: false,
+            success: function(pdelaymessages) {
+                this.setState({
+                    PDelayMessages: pdelaymessages,
+                });
+            }.bind(this)
+        })
     },
 
-    componentDidMount: function() {
-        this.loadAnnounce_MessagesFromServer();
-        setInterval(this.loadAnnounce_MessagesFromServer,
-                    this.props.pollInterval)
+    getInitialState() {
+        return {AnnounceMessages: [], PDelayMessages: []};
     },
-    */
-    render(){
 
-        return(
+    componentDidMount() {
 
-            <Provider store={store}>
-                <DashboardContainer/>
-            </Provider>
-    )
+        //this.loadAnnounce_MessagesFromServer();
+        //this.loadPDelay_MessagesFromServer();
+        setInterval(this.loadAnnounce_MessagesFromServer, 1000);
+        setInterval(this.loadPDelay_MessagesFromServer, 1000);
+
+    },
+
+
+    render() {
+
+        return (
+
+                <Router>
+                    <div>
+                    <NavigationBar/>
+                    <Switch>
+                        <Route exact path={'/'} component={() => <Announce_Message_Table data={this.state.AnnounceMessages}/>}/>
+                        <Route exact path={'/BMCA'} component={() => <BMCA data={this.state.AnnounceMessages[this.state.AnnounceMessages.length - 1]}/>}/>
+                        <Route exact path={'/Holdover'} component={()=> <Holdover data={this.state.AnnounceMessages[this.state.AnnounceMessages.length - 1]}/>}/>
+                        <Route exact path={'/LeapSecond'} component={() => <LeapSecond data={this.state.AnnounceMessages[this.state.AnnounceMessages.length - 1]}/>}/>
+                        <Route exact path={'/ATOI'} component={() => <ATOI data={this.state.AnnounceMessages[this.state.AnnounceMessages.length - 1]}/>}/>
+                        <Route exact path={'/MulticastMAC'} component={() => <MulticastMAC data={this.state.PDelayMessages[this.state.PDelayMessages.length - 1]}/>}/>
+                    </Switch>
+                    </div>
+                </Router>
+        )
     }
 
 })
 
-render(
+ render(
 
-    <Dashboard/>
+    <Dashboard />
   ,
     document.getElementById('Dashboard'));
 
 
-  /*Dashboard.propTypes = {
+  /*
+
+ render(
+
+    <Provider store={store}>
+    <Dashboard/>
+    </Provider>
+  ,
+    document.getElementById('Dashboard'));
+
+
+
+componentDidMount() {
+
+
+        setInterval(function(){
+            store.dispatch(announceMessageActions.fetchMessages())
+        }, 1000)
+
+        store.dispatch(announceMessageActions.fetchMessages())
+    }
+    Dashboard.propTypes = {
 
   children: React.PropTypes.object.isRequired
 
