1afecf5fa2fd7399e0cb2e1babfa365e36f275af
kevingbrady <kgb@nist.gov>
2017-09-21 13:02:10 -0400

    Delete fabfile.py

--- React-Redux-Django Dashboard/fabfile.py
+++
@@ -1,7 +0,0 @@
-from fabric.api import local
-
-def webpack():
-    local('rm -rf Dashboard/static/bundles/stage/*')
-    local('rm -rf Dashboard/static/bundles/prod/*')
-    local('webpack --config webpack.stage.config.js --progress --colors')
-    local('webpack --config webpack.prod.config.js --progress --colors')
