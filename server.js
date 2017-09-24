b1d7e98d9f903ceaf8bd5054fbc6be6002e6ae5b
kevingbrady <kgb@nist.gov>
2017-09-24 11:23:30 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kgb on 9/12/17.
+ */
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.local.config')
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  inline: true,
+  historyApiFallback: true,
+}).listen(3000, config.ip, function (err, result) {
+  if (err) {
+    console.log(err)
+  }
+
+  console.log('Listening at ' + config.ip + ':3000')
+})
