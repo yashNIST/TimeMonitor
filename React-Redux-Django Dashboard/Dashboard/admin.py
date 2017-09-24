4e4fb532e32e947516618647cde99b90143193be
kevingbrady <kgb@nist.gov>
2017-09-24 11:33:45 -0400

    Delete admin.py

--- React-Redux-Django Dashboard/Dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Announce_Message, Path_Delay_Request_Message
-
-admin.site.register(Announce_Message)
-admin.site.register(Path_Delay_Request_Message)
-
-
-# Register your models here.
