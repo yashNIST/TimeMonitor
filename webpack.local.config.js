26663edefefb1afb5acc47fce7c4130dba2b3826
kevingbrady <kgb@nist.gov>
2017-10-14 15:59:06 -0400

    Update webpack.local.config.js

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -4,32 +4,44 @@
 var path = require("path")
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
-
+var CompressionPlugin = require('compression-webpack-plugin');
+var ip = require('ip');
 var config = require('./webpack.base.config.js')
 
 config.devtool = "#eval-source-map"
 
-var ip = 'localhost'
+var myIP = ip.address()
 
 config.entry = {
   Timing_Testbed_Dashboard: [
-    'webpack-dev-server/client?http://' + ip + ':3000',
+    'webpack-dev-server/client?http://' + myIP + ':3000',
     'webpack/hot/only-dev-server',
     './ReactJS/Dashboard',
   ],
 }
 
-config.output.publicPath = 'http://' + ip + ':3000' + '/Dashboard/static/bundles/local/'
+config.output.publicPath = 'http://' + myIP + ':3000' + '/Dashboard/static/bundles/local/'
 
 config.plugins = config.plugins.concat([
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new BundleTracker({filename: './webpack-stats-local.json'}),
-  new webpack.DefinePlugin({
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new BundleTracker({filename: './webpack-stats-local.json'}),
+    new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('development'),
-      'BASE_API_URL': JSON.stringify('https://'+ ip +':8000/'),
-  }}),
+      'BASE_API_URL': JSON.stringify('https://'+ myIP +':8000/'),
+
+    }}),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
 ])
 
 config.module.loaders.push(
@@ -38,3 +50,4 @@ config.module.loaders.push(
 )
 
 module.exports = config
+
